b9dd1fbe776f38b00b052bab9d34b1a8fb5d4951
chanveasna044 <chanveasna044@gmail.com>
2023-05-25 09:59:31 +0700

    mini project next js

---
+++ src/app/page.js
@@ -0,0 +1,21 @@
+import Image from 'next/image'
+import { Suspense } from 'react'
+import LoadingDataFetching from './loading'
+import Products from './products/page'
+import Categories from './categories/page'
+import Users from './users/page'
+
+
+export default function Home() {
+  return (
+    <main className="bg-black flex min-h-screen flex-col items-center justify-between p-24">
+      <Suspense fallback={<LoadingDataFetching/>}>
+         
+          <Products/>
+          <Categories/>
+          <Users/>
+      </Suspense>
+
+    </main>
+  )
+}
